54082ad332969e613b7bd919c4e5f8ad0936f70d
Cholpon <januzakova.cholpon@gmail.com>
2026-06-27 21:09:17 +0600

    Refactor Pokemons component to use API for dynamic data and update styles for better layout

--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -1,49 +1,47 @@
-import React, { use } from 'react'
+import React, {  useEffect, useState } from 'react'
+
 import img1 from '../assets/bulbasaur.png'
 import img2 from '../assets/buterflay.png'
 import img3 from '../assets/ivysaur.jpeg'
 import img4 from '../assets/metapod.png'
 import img5 from '../assets/weedle.png'
 import img6 from '../assets/venusaur.png'
-
 import styles from '../styles/Pokemons.module.css'
+
 
 function Pokemons() {
 
-  const pokemons = [
-    {photo: img1,
-      name: 'Bulbasaur'
-    },
-    {photo: img2,
-      name: 'Butterflay'
-    },
-    {photo: img3,
-      name: 'Ivysaur'  
-    },
-    {photo: img4,
-      name: 'Metapod'
-    },
-    {photo: img5,
-      name: 'Weedle'
-    },
-    {photo: img6,
-      name: 'Venusaur'
+  const[ pokemons, setPokemons] = useState([])
+
+useEffect ( () => {
+  const getPokemon = async () =>{
+    
+ const response = await fetch ("https://pokeapi.co/api/v2/pokemon?limit=6");
+  const data = await response.json();
+
+  const updatedPokemons = data.results.map((pokemon, index)=>{
+    return{
+      name: pokemon.name,
+      image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
     }
-  ]
+  })
+
+  setPokemons(updatedPokemons)
+  }
+
+  getPokemon()
+},[])
 
 
 
-  
-  return (
+   return (
     <div className={styles.container}>
-      <div className={styles.pokemons}>
-        {pokemons.map((pokemon, index) => (
-          <div key={index} className={styles.pokemon}>
-            <img src={pokemon.photo} alt={pokemon.name} className={styles.img} />
-            <p>{pokemon.name}</p>
-          </div>
-        ))}
-      </div>
+      {pokemons.map((pokemon, index) => (
+        <div key={index} className={styles.pokemon}>
+          <img src={pokemon.image} alt={pokemon.name} />
+          <p>{pokemon.name}</p>
+        </div>
+      ))}
     </div>
   )
 }
